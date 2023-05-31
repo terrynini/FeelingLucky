ca13ea740b002189e053c493b3a68ce76b0ecb08
sud0woodo <40278342+sud0woodo@users.noreply.github.com>
2023-06-01 01:31:50 +0900

    We need true randomness

--- FeelingLucky.py
+++ FeelingLucky.py
@@ -1,20 +1,34 @@
 import random
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QAction
+
+# External dependencies
+import httpx
 import ida_kernwin
 import idaapi
 import idautils
-import os
+
+
+try:
+    # Generate true randomness if we got a connection to the outside world
+    drand_info = httpx.get("https://api.drand.sh/public/latest").json()
+    seed = int(drand_info["randomness"], 16)
+    random.seed(seed)
+except httpx.ConnectError:
+    # Use the default random seed otherwise
+    pass
+
 
 class LuckyHandler(idaapi.action_handler_t):
     def __init__(self):
         idaapi.action_handler_t.__init__(self)
+
     def activate(self, ctx):
         functions = idautils.Functions()
         function_ea = random.choice(list(functions))
         return idaapi.jumpto(function_ea)
+
     def update(self, ctx):
         return idaapi.AST_ENABLE_ALWAYS
+
 
 class LuckyPlugin(idaapi.plugin_t):
     flags = idaapi.PLUGIN_PROC
@@ -24,19 +38,14 @@ class LuckyPlugin(idaapi.plugin_t):
 
     def init(self):
         idahome = idaapi.idadir("plugins/FeelingLucky")
-        icon_data = open(idahome+"/dice_icon.png", "rb").read()
+        icon_data = open(idahome + "/dice_icon.png", "rb").read()
         act_icon = ida_kernwin.load_custom_icon(data=icon_data, format="png")
         action_desc = ida_kernwin.action_desc_t(
-            'nini:FeelingLucky',
-            'Trying your luck',
-            LuckyHandler(),
-            'Alt+Z',
-            'Trying your luck',
-            act_icon
+            "nini:FeelingLucky", "Trying your luck", LuckyHandler(), "Alt+Z", "Trying your luck", act_icon
         )
 
         ida_kernwin.register_action(action_desc)
-        idaapi.attach_action_to_toolbar("AnalysisToolBar", 'nini:FeelingLucky')
+        idaapi.attach_action_to_toolbar("AnalysisToolBar", "nini:FeelingLucky")
 
         return idaapi.PLUGIN_KEEP
 
@@ -46,5 +55,6 @@ class LuckyPlugin(idaapi.plugin_t):
     def term(self):
         ida_kernwin.unregister_action("nini:FeelingLucky")
 
+
 def PLUGIN_ENTRY():
-    return LuckyPlugin() 
+    return LuckyPlugin()
